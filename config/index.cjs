0ef443ea243f3a8d4d05e2a6fd5dd8b8b03f8b70
yhhhhhz <151590478+yhhhhhz@users.noreply.github.com>
2023-11-21 09:44:14 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,19 +21,15 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '宝贝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
+      id: 'zmj040100',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '',
+      useTemplateId: 'zmj040100',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '12-27',
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {
           type: '*生日', name: '宝贝', year: '2000', date: '04-01',
-        },
-        // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
-        {
-          type: '生日', name: '李四', year: '1996', date: '09-31',
         },
         {
           type: '节日', name: '相识纪念日', year: '2023', date: '02-05',
@@ -43,15 +39,13 @@ const USER_CONFIG = {
       customizedDateList: [
         // 在一起的日子
         { keyword: 'love_day', date: '2023-02-05' },
-        // 结婚纪念日
-        { keyword: 'marry_day', date: '2022-09-09' },
       ],
     },
   ],
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: '',
+  CALLBACK_TEMPLATE_ID: 'Godblessyhz',
 
   CALLBACK_USERS: [
     {
